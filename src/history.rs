5bae2d5cebec6d241896338777eec7390f5d9dd9
XantX <hp_xantx@hotmail.com>
2024-02-03 13:30:03 -0500

    First part of gow until kill his family

--- src/history.rs
+++ src/history.rs
@@ -84,11 +84,77 @@ fn create_history() -> TreeNode {
                             String::from("Sword of caos")
                           ]);
     line10.do_action = true;
-    let line11 = new_node(11,
-                          String::from("Siguiente capitulo"),
+
+    let mut line11 = new_node(11,
+                          String::from("The weapons of chaos melted in the arms of kratos and with the swords he defeated the king of the barbarians."),
                           HashMap::new(),
                           Vec::new());
 
+    let mut line12 = new_node(12,
+                          String::from("From that day on, General Kratos became the emissary of destruction on behalf of Ares."),
+                          HashMap::new(),
+                          Vec::new());
+
+    let mut line13 = new_node(13,
+                          String::from("In the name of Ares, he killed innocents, plundered countless cities and destroyed empires."),
+                          HashMap::new(),
+                          Vec::new());
+
+    let mut line14 = new_node(14,
+                          String::from("One day Ares sent Kratos to destroy a temple dedicated to Athena."),
+                          HashMap::new(),
+                          Vec::new());
+
+    let mut line15 = new_node(15,
+                          String::from("As he stood in front of the temple, Kratos had a feeling. Something told him that he should not cross the gates of that temple."),
+                          HashMap::new(),
+                          Vec::new());
+
+    let mut line16 = new_node(16,
+                          String::from("The temple oracle warns you that the dangers inside the temple are greater than you can imagine."),
+                          HashMap::from([
+                            (0, String::from("1) Kratos enters the temple")),
+                            (1, String::from("2) Kratos does not enter the temple"))
+                          ]),
+                          Vec::new());
+
+    // First final: Kratos don't kill his family
+    let line18 = new_node(18,
+                          String::from("Kratos raises his gaze to the sky and sees Ares in the clouds."),
+                          HashMap::new(),
+                          Vec::new());
+
+    let mut line17 = new_node(17,
+                          String::from("Kratos enters the temple and kills every man, woman and child that crosses his path."),
+                          HashMap::new(),
+                          Vec::new());
+
+    let mut line19 = new_node(19,
+                          String::from("Not realizing that among the innocents were his wife Lysandra and daughter Kaliope."),
+                          HashMap::new(),
+                          Vec::new());
+
+    let mut line20 = new_node(20,
+                          String::from("Repentant for the act he has just committed, Kratos swears revenge against the god who has entrusted him with this task."),
+                          HashMap::new(),
+                          Vec::new());
+
+    let line21 = new_node(21,
+                          String::from("Upon leaving the temple, the Oracle of Athena cursed Kratos by painting his body with the ashes of his family."),
+                          HashMap::new(),
+                          Vec::new());
+
+    line20.childen.push(line21);
+    line19.childen.push(line20);
+    line17.childen.push(line19);
+    line16.childen.push(line17);
+    line16.childen.push(line18);
+
+    line15.childen.push(line16);
+    line14.childen.push(line15);
+    line13.childen.push(line14);
+    line12.childen.push(line13);
+    line11.childen.push(line12);
     line10.childen.push(line11);
     line9.childen.push(line10);
     line7.childen.push(line9);
