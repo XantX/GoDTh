a1b9f98415435a7a135b5bdd886aae7d333a7cf4
XantX <hp_xantx@hotmail.com>
2024-02-03 11:07:55 -0500

    Modify option selection

--- src/history.rs
+++ src/history.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::character::Kratos;
 
 #[derive(Debug)]
@@ -5,12 +7,13 @@ pub struct TreeNode {
     pub line: String,
     pub value: i32,
     pub childen: Vec<TreeNode>,
-    pub decisions: Vec<String>,
+    pub decisions: HashMap<i32, String>,
     pub object_granted: Vec<String>,
+    pub do_action: bool,
 }
 
-fn new_node(value: i32, line: String, decisions: Vec<String>, object_granted: Vec<String>) -> TreeNode {
-    TreeNode { line, value, childen: Vec::new(), decisions, object_granted}
+fn new_node(value: i32, line: String, decisions: HashMap<i32, String>, object_granted: Vec<String>) -> TreeNode {
+    TreeNode { line, value, childen: Vec::new(), decisions, object_granted, do_action: false}
 }
 
 impl TreeNode {
@@ -25,64 +28,65 @@ fn create_history() -> TreeNode {
     // Presentacion
     let mut root = new_node(0,
                             String::from("Fan-adapted story: XantX"),
-                            Vec::new(),
+                            HashMap::new(),
                             Vec::new());
 
     // History part 1 
     let mut line1 = new_node(1,
                              String::from("Long ago there was a Spartan general named kratos dedicated to the art of war and combat."),
-                             Vec::new(),
+                             HashMap::new(),
                              Vec::new());
     let mut line2 = new_node(2,
                              String::from("Wherever he went, he caused chaos and destruction to his enemies."),
-                             Vec::new(),
+                             HashMap::new(),
                              Vec::new());
     let mut line3 = new_node(3,
                              String::from("One day his army faced a tough opponent."),
-                             Vec::new(),
+                             HashMap::new(),
                              Vec::new());
     let mut line4 = new_node(4,
                              String::from("The barbarian army"),
-                             Vec::new(),
+                             HashMap::new(),
                              Vec::new());
     let mut line5 = new_node(5,
                              String::from("The Barbarons mercilessly ravaged the Spartan army."),
-                             Vec::new(),
+                             HashMap::new(),
                              Vec::new());
     let mut line6 = new_node(6,
                              String::from("Cornered, Kratos had to make a decision."),
-                             vec![
-                                String::from("1) Invoke Ares, god of war"),
-                                String::from("2) Accept your destiny")
-                             ],
+                             HashMap::from([
+                                (0, String::from("1) Invoke Ares, god of war")),
+                                (1, String::from("2) Accept your destiny"))
+                             ]),
                              Vec::new());
 
     // First end
     let line8 = new_node(8,
                          String::from("Kratos takes an axe from the Bavarian general and dies in battle"),
-                         Vec::new(),
+                         HashMap::new(),
                          Vec::new());
 
 
     // Second line
     let mut line7 = new_node(7,
                              String::from("Kratos shouts to the heavens the deal for Ares."),
-                             Vec::new(),
+                             HashMap::new(),
                              Vec::new());
     let mut line9 = new_node(9,
                              String::from("Kratos promises that if he gives him the power to defeat his enemies, he will be his servant for all eternity."),
-                             Vec::new(),
+                             HashMap::new(),
                              Vec::new());
 
     let mut line10 = new_node(10,
                           String::from("Ares came down from Olympus to defeat the barbarians and bestowed upon Kratos the 'Swords of Chaos' and melted into Kratos' arms."),
-                          Vec::new(),
+                          HashMap::new(),
                           vec![
                             String::from("Sword of caos")
                           ]);
+    line10.do_action = true;
     let line11 = new_node(11,
                           String::from("Siguiente capitulo"),
-                          Vec::new(),
+                          HashMap::new(),
                           Vec::new());
 
     line10.childen.push(line11);
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-mod history;
 mod character;
+mod history;
 
 use history::return_history;
 use history::TreeNode;
@@ -8,8 +8,7 @@ use std::io::{self, Read, Write};
 
 use crate::character::Kratos;
 
-
-fn grant_sword_of_chaos(kratos: &Kratos, weapons: &Vec<String>) -> Kratos {
+fn grant_weapon(kratos: &Kratos, weapons: &Vec<String>) -> Kratos {
     let mut new_kratos: Kratos = kratos.clone();
     for weapon in weapons {
         new_kratos.weapon.push(weapon.to_string());
@@ -31,7 +30,7 @@ fn get_child(node: &mut TreeNode) -> Option<&mut TreeNode> {
 }
 
 fn show_desicions(node: &TreeNode) {
-    for desicion in &node.decisions {
+    for (_option, desicion) in node.decisions.iter() {
         println!("{desicion}");
     }
 }
@@ -39,14 +38,16 @@ fn show_desicions(node: &TreeNode) {
 fn main() {
     let mut history_tree: history::TreeNode = return_history();
     let mut current_node = &mut history_tree;
-    let next_line = "\n\n\nPress 'n' to continue.";
+    let next_line = "\n\n\nPress 'n' to continue.\nPress 'c' to show character info.";
     let option_line = "\n\n\nWrite the option to continue.";
     let mut is_option_moment: bool;
-    let mut kratos = Kratos::new(Vec::new(), 10, Vec::new(),100);
+    let mut kratos = Kratos::new(Vec::new(), 10, Vec::new(), 100);
     loop {
         clear_terminal();
         println!("{}", current_node.line);
-        kratos = current_node.action(grant_sword_of_chaos, &kratos);
+        if current_node.do_action {
+            kratos = current_node.action(grant_weapon, &kratos);
+        }
         if current_node.decisions.len() > 1 {
             show_desicions(current_node);
             is_option_moment = true;
@@ -61,12 +62,18 @@ fn main() {
         }
 
         if let Some(input) = read_key() {
-            if input == '1' && is_option_moment {
-                current_node = &mut current_node.childen[0]
+            if is_option_moment {
+                let option_selected = match input.to_digit(10) {
+                    Some(n) if n - 1 < current_node.childen.len() as u32 => n as usize,
+                    _ => {
+                        println!("Opción inválida.");
+                        continue;
+                    }
+                };
+                println!("{option_selected}");
+                current_node = &mut current_node.childen[option_selected - 1];
             }
-            if input == '2' && is_option_moment {
-                current_node = &mut current_node.childen[1]
-            }
+
             if input == 'n' && !is_option_moment {
                 if let Some(child_node) = get_child(current_node) {
                     current_node = child_node;
@@ -85,19 +92,17 @@ fn main() {
         }
     }
     println!("Fin del juego.")
-
 }
 
 fn read_key() -> Option<char> {
     let mut buffer = [0; 1];
     match io::stdin().read_exact(&mut buffer) {
         Ok(_) => {
-            // Limpiar el buffer de entrada para evitar leer caracteres adicionales
             let stdin = io::stdin();
             let mut lock = stdin.lock();
             let _ = lock.read_until(b'\n', &mut Vec::new());
             Some(buffer[0] as char)
-        },
+        }
         Err(_) => None,
     }
 }
